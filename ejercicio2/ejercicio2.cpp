618ce1f13495bb860d3ad0a7d4ac53c5e9e5f446
Belen casas <mcasas@udesa.edu.ar>
2025-03-13 19:58:05 -0300

    parte del ejercicio 2

--- ejercicio2/ejercicio2.cpp
+++ ejercicio2/ejercicio2.cpp
@@ -4,51 +4,49 @@
 
 using namespace std;
 
-/* En este ejercicio, se pide crear un sistema log que permite agregar entradas a un
-archivo mediante el llamado a una función logMessage definida en pseudo código de
-la siguiente manera:
-void logMessage(String mensaje, Integer/Otro NivelSeveridad)
-Donde NivelSeveridad corresponderá con unas de las leyendas previamente
-mencionadas. El formato esperado en una línea del archivo de log es el siguiente:
+//A)
 
-[ERROR] <Mensaje>
-[INFO] <Mensaje>
-etc.
-Verifique su funcionamiento con al menos una entrada de cada tipo.*/
+enum class severidad {DEBUG = 1 , INFO , WARNING , ERROR , CRITICAL};
 
-enum class severidad {DEBUG = 1 , INFO , WARNING , ERROS , CRITICAL};
-
-void logmessage(string mensaje, int NivelSeveridad){
-    int NivelSeveridad = 5;
-    switch (NivelSeveridad)
+string nivelseveridad (severidad nivel){
+    switch (nivel)
     {
-    case 1: 
-    
-       
+    case severidad::DEBUG: return "[DEBUG]";
+    case severidad::INFO: return "[INFO]";
+    case severidad::WARNING: return "[WARNING]";
+    case severidad::ERROR: return "[ERROR]";
+    case severidad::CRITICAL: return "[CRITICAL]";
+    default: return "[UNKNOWN]";
+      
     }
-
-
 }
 
-void creararchivo(){
-    ofstream outFile("archivo_de_texto.txt", ios::app);
-
+void logmessage(string mensaje, severidad nivel){
+    ofstream outFile("archivo_de_texto.txt" , ios::app); 
     if(!outFile.is_open()){
-        cerr << "No se pudo crear el archivo de salida." << endl;
-        return 1;
-
+        cerr << "No se pudo abrir el archivo pra escribir." << endl;
+        return;
     }
+    outFile << nivelseveridad(nivel) << "<"<< mensaje << ">" << endl;  
+    outFile.close(); 
 }
 
 int main(){
     cout << "Ingrese su mensaje: "; 
-    string msj;
-    cin.ignore();
-    getline(cin, msj);
+    string msj; 
+    getline(cin, msj); 
+
     cout << " 1-DEBUG \n 2-INFO \n 3-WARNING \n 4-ERROR \n 5-CRITICAL\n";
     cout << endl << "Que opcion quiere utilizar: ";
     int level;
     cin >> level; 
 
-    logmessage(msj, level)
+    cin.ignore();  
+
+    severidad nivel = static_cast<severidad>(level); 
+
+    logmessage(msj, nivel);
+
+    cout<< "El mensaje se registro correctamente." << endl;
+    return 0; 
 }
